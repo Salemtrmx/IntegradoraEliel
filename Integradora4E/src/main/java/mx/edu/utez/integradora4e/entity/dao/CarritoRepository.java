ee3228c1f4745831ea827c6db1ca44fed85e4a89
Salem <20233tn096@utez.edu.mx>
2024-12-02 16:09:53 -0600

    subo CarritoRepository

---
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/CarritoRepository.java
@@ -0,0 +1,7 @@
+package mx.edu.utez.integradora4e.entity.dao;
+
+import mx.edu.utez.integradora4e.entity.CarritoProducto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarritoRepository extends JpaRepository<CarritoProducto, Integer> {
+}
