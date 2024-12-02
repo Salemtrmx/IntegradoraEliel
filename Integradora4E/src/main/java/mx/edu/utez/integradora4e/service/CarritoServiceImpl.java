4317b46979c19c4ee2cf3afbe6dd47db52a544d7
Salem <20233tn096@utez.edu.mx>
2024-12-02 16:10:37 -0600

    subo CarritoServiceImpl

---
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/CarritoServiceImpl.java
@@ -0,0 +1,24 @@
+package mx.edu.utez.integradora4e.service;
+
+import mx.edu.utez.integradora4e.entity.CarritoProducto;
+import mx.edu.utez.integradora4e.entity.dao.CarritoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarritoServiceImpl implements  CarritoService{
+
+    private final CarritoRepository carritoRepository;
+    public CarritoServiceImpl(CarritoRepository carritoRepository) {this.carritoRepository = carritoRepository;}
+
+    @Override
+    public List<CarritoProducto> getCarritos() {
+        return carritoRepository.findAll();
+    }
+
+    @Override
+    public CarritoProducto addCarrito(CarritoProducto carrito) {
+        return carritoRepository.save(carrito);
+    }
+}
