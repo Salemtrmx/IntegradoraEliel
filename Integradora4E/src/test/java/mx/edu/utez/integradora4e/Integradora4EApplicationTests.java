4c7741ce72643a09edc117fa8e44c35764c39980
Salemtrmx <20233tn096@utez.edu.mx>
2024-12-02 11:30:49 -0600

    Subo el proyecto a git

---
+++ Integradora4E/src/test/java/mx/edu/utez/integradora4e/Integradora4EApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.integradora4e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradora4EApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
