c6d175734bd123fcf90dbc7d95666659718ce272
Salem <20233tn096@utez.edu.mx>
2024-12-10 17:47:15 -0600

    Agrego en CarritoRepository el delete por Id

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/CarritoRepository.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/CarritoRepository.java
@@ -4,4 +4,6 @@ import mx.edu.utez.integradora4e.entity.CarritoProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarritoRepository extends JpaRepository<CarritoProducto, Integer> {
+    static void deleteById(Long id) {
+    }
 }
