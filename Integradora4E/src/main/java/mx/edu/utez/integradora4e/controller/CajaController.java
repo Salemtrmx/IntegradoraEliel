ec5290fb27aebce163783e7619920d38d121f6b4
CA3-SEP <20233tn114@utez.edu.mx>
2024-12-10 19:36:31 -0600

    agrego caja controller para agrega un cliente

---
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/CajaController.java
@@ -0,0 +1,21 @@
+package mx.edu.utez.integradora4e.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@RestController
+@RequestMapping("/caja")
+class CajaController {
+
+    private final Queue<String> filaClientes = new LinkedList<>();
+
+    // Endpoint para agregar un cliente a la fila
+    @PostMapping("/agregar")
+    public String agregarCliente(@RequestParam String cliente) {
+        filaClientes.add(cliente);
+        return "Cliente agregado: " + cliente;
+    }
+}
