b05b216f17279f6ba6e0269a3f81316570a55ebb
CA3-SEP <20233tn114@utez.edu.mx>
2024-12-10 19:40:08 -0600

    Corrigo los endpoits y abtener fila

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/CajaController.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/CajaController.java
@@ -6,22 +6,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
 @RestController
 @RequestMapping("/caja")
 class CajaController {
 
     private final Queue<String> filaClientes = new LinkedList<>();
 
-    // Endpoint para agregar un cliente a la fila
     @PostMapping("/agregar")
     public String agregarCliente(@RequestParam String cliente) {
         filaClientes.add(cliente);
         return "Cliente agregado: " + cliente;
     }
 
-    // Endpoint para mostrar los clientes en la fila
-    @GetMapping("/fila")
+    @GetMapping("/atender")
+    public String atenderCliente() {
+        if (filaClientes.isEmpty()) {
+            return "No hay clientes en la fila.";
+        }
+        String clienteAtendido = filaClientes.poll();
+        return "Cliente atendido: " + clienteAtendido;
+    }
+
+    @GetMapping("/obtenerFila")
     public Queue<String> obtenerFila() {
         return filaClientes;
     }
-    }
+}
+
