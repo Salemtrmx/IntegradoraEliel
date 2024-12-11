44ffd6599cf9d8f67a370c72dbcd456460e04440
CA3-SEP <20233tn114@utez.edu.mx>
2024-12-10 19:56:18 -0600

    agrego metododo findById

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/ClienteRepository.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/ClienteRepository.java
@@ -3,5 +3,8 @@ package mx.edu.utez.integradora4e.entity.dao;
 import mx.edu.utez.integradora4e.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Optional<Cliente> findById(Long id);
 }
