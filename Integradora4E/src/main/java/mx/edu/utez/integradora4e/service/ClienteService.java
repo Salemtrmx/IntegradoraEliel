088007403be8c018a57eae39296ff282ff2c59f0
CA3-SEP <20233tn114@utez.edu.mx>
2024-12-10 19:56:50 -0600

    agrego metododo obtenerClientePorId

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/ClienteService.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/ClienteService.java
@@ -1,10 +1,14 @@
 package mx.edu.utez.integradora4e.service;
 
 import mx.edu.utez.integradora4e.entity.Cliente;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 public interface ClienteService {
     List<Cliente> listarClientes();
     Cliente guardarCliente(Cliente cliente);
+
+    Cliente obtenerClientePorId(Long id);
 }
