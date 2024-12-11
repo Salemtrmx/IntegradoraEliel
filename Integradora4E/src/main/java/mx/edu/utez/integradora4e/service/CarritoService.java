d282aa86e06081b6b29ecd2ad7939609cb84b99d
Salem <20233tn096@utez.edu.mx>
2024-12-10 20:01:16 -0600

    añado buscar carrito por ID

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/CarritoService.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/CarritoService.java
@@ -7,7 +7,6 @@ import java.util.List;
 public interface CarritoService {
     List<CarritoProducto> getCarritos();
     CarritoProducto addCarrito(CarritoProducto carrito);
+    CarritoProducto buscarCarritoPorId(int id);
     void borrarCarrito(Long id);
-
-
 }
