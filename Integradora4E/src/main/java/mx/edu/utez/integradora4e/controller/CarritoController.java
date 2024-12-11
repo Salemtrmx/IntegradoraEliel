0c9ce8877c9e2acb285c35f6e874b2f400b7304e
Salem <20233tn096@utez.edu.mx>
2024-12-10 18:45:08 -0600

    Hice en el controller el borrar carrito

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/CarritoController.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/CarritoController.java
@@ -16,15 +16,29 @@ public class CarritoController {
 
     public CarritoController(CarritoService carritoService) {this.carritoService = carritoService;}
 
+    //PARA LISTAR CARRITOS
     @GetMapping
     public ResponseEntity<ApiResponse<List<CarritoProducto>>> listarCarritos() {
         List<CarritoProducto> carritos = carritoService.getCarritos();
         return ResponseEntity.ok(new ApiResponse<>("Se han obtenido los carritos disponibles", carritos));
     }
 
+    //PARA AGREGAR UN CARRITO
     @PostMapping
     public ResponseEntity<ApiResponse<CarritoProducto>> agregarCarrito(@RequestBody CarritoProducto producto) {
         CarritoProducto nuevoCarrito = carritoService.addCarrito(producto);
         return ResponseEntity.ok(new ApiResponse<>("Se ha agregado un carrito exitosamente", nuevoCarrito));
     }
+
+
+    //PARA BORRAR UN CARRITO
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<CarritoProducto>> eliminarCarrito(@PathVariable Long id) {
+        carritoService.borrarCarrito(id);
+        return ResponseEntity.ok(new ApiResponse<>("Se ha eliminado un carrito", null));
+    }
+
+
+
+
 }
