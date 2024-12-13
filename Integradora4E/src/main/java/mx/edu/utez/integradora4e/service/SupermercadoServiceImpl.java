7011d2a18f33786a5e2f06882ce189c3e6b5a671
Dell 3020 <20233tn114@utez.edu.mx>
2024-12-12 20:38:51 -0600

    le puse validacion al procesar compra

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoServiceImpl.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoServiceImpl.java
@@ -11,6 +11,9 @@ public class SupermercadoServiceImpl implements SupermercadoService {
 
     @Override
     public String procesarCompra(Long clienteId, List<CarritoProducto> productos) {
+        if (productos == null || productos.isEmpty()) {
+            return "El carrito está vacío, no se puede procesar la compra.";
+        }
         return "compra realizada con exito";
     }
 }
