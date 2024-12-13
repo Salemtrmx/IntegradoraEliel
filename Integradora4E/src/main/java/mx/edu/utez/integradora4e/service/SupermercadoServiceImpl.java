edef56650b495b20fc8aad446fe095d8d33fbd2d
Dell 3020 <20233tn114@utez.edu.mx>
2024-12-12 21:04:25 -0600

    agrego en SupermercadoServiceImpl para encontrar cliente id

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoServiceImpl.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoServiceImpl.java
@@ -2,6 +2,8 @@ package mx.edu.utez.integradora4e.service;
 
 
 import mx.edu.utez.integradora4e.entity.CarritoProducto;
+import mx.edu.utez.integradora4e.entity.Cliente;
+import mx.edu.utez.integradora4e.entity.dao.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,11 +11,21 @@ import java.util.List;
 @Service
 public class SupermercadoServiceImpl implements SupermercadoService {
 
+    private final ClienteRepository clienteRepository;
+
+    public SupermercadoServiceImpl(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
     @Override
     public String procesarCompra(Long clienteId, List<CarritoProducto> productos) {
         if (productos == null || productos.isEmpty()) {
             return "El carrito está vacío, no se puede procesar la compra.";
         }
-        return "compra realizada con exito";
+
+        Cliente cliente = clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente no encontrado"));
+
+        return "Compra procesada exitosamente.";
     }
 }
