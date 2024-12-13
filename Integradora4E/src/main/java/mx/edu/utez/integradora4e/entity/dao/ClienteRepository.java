9819057791120cdcf2ac6e249c8addd7bd46434a
Salemtrmx <20233tn096@utez.edu.mx>
2024-12-12 20:33:41 -0600

    Delete Integradora4E directory

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.edu.utez.integradora4e.entity.dao;
-
-import mx.edu.utez.integradora4e.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.edu.utez.integradora4e.entity.dao;
-
-import mx.edu.utez.integradora4e.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
