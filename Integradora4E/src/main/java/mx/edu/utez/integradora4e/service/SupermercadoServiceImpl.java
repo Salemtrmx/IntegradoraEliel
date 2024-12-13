e8d4b5f1096e5adc1f937a7936a09438c013c59a
Dell 3020 <20233tn114@utez.edu.mx>
2024-12-12 21:18:06 -0600

    agrego en SupermercadoServiceImpl para que haga la funcion de total

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoServiceImpl.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoServiceImpl.java
@@ -3,6 +3,7 @@ package mx.edu.utez.integradora4e.service;
 
 import mx.edu.utez.integradora4e.entity.CarritoProducto;
 import mx.edu.utez.integradora4e.entity.Cliente;
+import mx.edu.utez.integradora4e.entity.Producto;
 import mx.edu.utez.integradora4e.entity.dao.ClienteRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Service
 public class SupermercadoServiceImpl implements SupermercadoService {
-
+Producto p = new Producto();
     private final ClienteRepository clienteRepository;
 
     public SupermercadoServiceImpl(ClienteRepository clienteRepository) {
@@ -21,6 +22,10 @@ public class SupermercadoServiceImpl implements SupermercadoService {
     public String procesarCompra(Long clienteId, List<CarritoProducto> productos) {
         if (productos == null || productos.isEmpty()) {
             return "El carrito está vacío, no se puede procesar la compra.";
+        }
+        double total = 0;
+        for (CarritoProducto producto : productos) {
+            total += p.getPrecio() * producto.getCantidad();
         }
 
         Cliente cliente = clienteRepository.findById(clienteId)
