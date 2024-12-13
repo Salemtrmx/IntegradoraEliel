d7be8319e27d7717f2de17471905beb3c5c19a30
Salemtrmx <20233tn096@utez.edu.mx>
2024-12-12 20:27:38 -0600

    Delete Integradora4E directory

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/CarritoRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.edu.utez.integradora4e.entity.dao;
-
-import mx.edu.utez.integradora4e.entity.CarritoProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarritoRepository extends JpaRepository<CarritoProducto, Integer> {
-}
