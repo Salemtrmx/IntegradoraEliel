2ab62e780945d2972ece78b633cb972cc814a315
Salem <20233tn096@utez.edu.mx>
2024-12-14 08:19:35 -0600

    quito lo de listar producto (no se ocupa)

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/ProductoService.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/ProductoService.java
@@ -5,6 +5,5 @@ import mx.edu.utez.integradora4e.entity.Producto;
 import java.util.List;
 
 public interface ProductoService {
-    List<Producto> listarProducto();
     Producto guardarProducto(Producto producto);
 }
