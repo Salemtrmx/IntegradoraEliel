fe09f052aebb3b5685839c245fc00de251448845
Salem <20233tn096@utez.edu.mx>
2024-12-14 12:19:37 -0600

    simon

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/ProductoRepository.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/entity/dao/ProductoRepository.java
@@ -3,5 +3,6 @@ package mx.edu.utez.integradora4e.entity.dao;
 import mx.edu.utez.integradora4e.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
