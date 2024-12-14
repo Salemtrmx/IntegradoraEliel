7167538e649293551aa5af697f9a9d0db79491aa
Salem <20233tn096@utez.edu.mx>
2024-12-14 08:18:58 -0600

    imports

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/ProductoController.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/controller/ProductoController.java
@@ -4,6 +4,8 @@ import mx.edu.utez.integradora4e.entity.Producto;
 import mx.edu.utez.integradora4e.response.ApiResponse;
 import mx.edu.utez.integradora4e.service.ProductoService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
