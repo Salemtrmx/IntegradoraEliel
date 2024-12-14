a5f204c5db7e559ddc830f0b1dbe0b3d6434323f
Salem <20233tn096@utez.edu.mx>
2024-12-14 12:21:07 -0600

    yaaa

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/Integradora4EApplication.java
+++ Integradora4E/src/main/java/mx/edu/utez/integradora4e/Integradora4EApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Integradora4EApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Integradora4EApplication.class, args);
     }
