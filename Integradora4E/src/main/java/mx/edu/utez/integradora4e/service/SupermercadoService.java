dc8b8a01e283f3c25f5a16191e2d0b048b8fe7c1
Salem <20233tn096@utez.edu.mx>
2024-12-14 08:21:36 -0600

    cambios leves

--- Integradora4E/src/main/java/mx/edu/utez/integradora4e/service/SupermercadoService.java
+++
@@ -1,9 +0,0 @@
-package mx.edu.utez.integradora4e.service;
-
-import mx.edu.utez.integradora4e.entity.CarritoProducto;
-
-import java.util.List;
-
-public interface SupermercadoService {
-    String procesarCompra(Long clienteId, List<CarritoProducto> productos);
-}
